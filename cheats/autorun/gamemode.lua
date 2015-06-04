212064e240def41261398a8ef1b2f6e92468411e
Mark Dietzer <mriq91@gmail.com>
2015-06-04 15:50:36 +0200

    Make /gm bcast

--- cheats/autorun/gamemode.lua
+++ cheats/autorun/gamemode.lua
@@ -8,7 +8,8 @@ Command:register{
 	aliases = {"gm"}, 
 	action = {
 		format = "%s set %s gamemode to %s",
-		isProperty = true
+		isProperty = true,
+		broadcast = true
 	},
 	arguments = {
 		{
@@ -30,8 +31,10 @@ Command:register{
 			immunityRequirement = Permission.immunity.GREATER
 		}
 	},
-	run = function(self, ply, args)
+	run = function(self, ply, args, flags)
 		args.target:setGameMode(args.gamemode)
-		self:sendActionReply(ply, args.target, nil, args.gamemode:name())
+		self:sendActionReply(ply, args.target, {
+			silent = flags:contains("s")
+		}, args.gamemode:name())
 	end
 }
