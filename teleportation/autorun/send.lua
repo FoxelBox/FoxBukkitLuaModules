a3600de81304343493d1db84415931a7f63a2099
Mark Dietzer <mriq91@gmail.com>
2015-06-04 20:56:50 +0200

    Teleportation

---
+++ teleportation/autorun/send.lua
@@ -0,0 +1,65 @@
+local Command = require("Command")
+local Permission = require("Permission")
+
+local Location = luajava.bindClass("org.bukkit.Location")
+
+Command:register{
+	name = "send",
+	action = {
+		format = "%s teleported %s to %s",
+		isProperty = false
+	},
+	arguments = {
+		{
+			name = "from_target",
+			type = "player",
+			required = true,
+			immunityRequirement = Permission.immunity.GREATER
+		},
+		{
+			name = "to_target",
+			type = "player",
+			required = true,
+			flagsForbidden = "c",
+			immunityRequirement = Permission.immunity.GREATER_OR_EQUAL
+		},
+		{
+			name = "x",
+			type = "number",
+			required = true,
+			flagsRequired = "c"
+		},
+		{
+			name = "yz",
+			type = "number",
+			required = true,
+			flagsRequired = "c"
+		},
+		{
+			name = "z",
+			type = "number",
+			required = false,
+			default = false,
+			flagsRequired = "c"
+		}
+	},
+	run = function(self, ply, args, flags)
+		if flags:contains("c") then
+			local world = ply:getWorld()
+			
+			local x = args.x
+			local z = args.z or args.yz
+
+			local y = args.z and args.yz or world:getHighestBlockYAt(x, z)
+			
+			local location = luajava.new(Location, world, x, y, z)
+			args.from_target:teleport(location)
+			self:sendActionReply(ply, args.from_target, {
+				format = "%s teleported %s to %d %d %d"
+			}, x, y, z)
+			return
+		end
+		args.from_target:teleport(args.to_target.__entity)
+		self:sendActionReply(ply, args.from_target, {}, args.to_target)
+	end
+}
---
+++ teleportation/autorun/tp.lua
@@ -0,0 +1,61 @@
+local Command = require("Command")
+local Permission = require("Permission")
+
+local Location = luajava.bindClass("org.bukkit.Location")
+
+Command:register{
+	name = "tp",
+	action = {
+		format = "%s teleported to %s",
+		isProperty = false
+	},
+	arguments = {
+		{
+			name = "target",
+			type = "player",
+			required = true,
+			flagsForbidden = "c",
+			immunityRequirement = Permission.immunity.GREATER_OR_EQUAL
+		},
+		{
+			name = "x",
+			type = "number",
+			required = true,
+			flagsRequired = "c"
+		},
+		{
+			name = "yz",
+			type = "number",
+			required = true,
+			flagsRequired = "c"
+		},
+		{
+			name = "z",
+			type = "number",
+			required = false,
+			default = false,
+			flagsRequired = "c"
+		}
+	},
+	run = function(self, ply, args, flags)
+		if flags:contains("c") then
+			local world = ply:getWorld()
+			
+			local x = args.x
+			local z = args.z or args.yz
+
+			local y = args.z and args.yz or world:getHighestBlockYAt(x, z)
+			
+			local location = luajava.new(Location, world, x, y, z)
+			ply:teleport(location)
+			self:sendActionReply(ply, ply, {
+				format = "%s teleported %s to %d %d %d"
+			}, x, y, z)
+			return
+		end
+		ply:teleport(args.target.__entity)
+		self:sendActionReply(ply, args.target, {
+			silentToTarget = flags:contains("s")
+		})
+	end
+}
