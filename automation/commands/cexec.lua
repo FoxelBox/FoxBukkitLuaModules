e78c4212a7c8561a7b1d4c29f533162b06abd380
Mark Dietzer <mriq91@gmail.com>
2015-06-28 00:47:29 +0200

    God damn me xD

--- automation/commands/cexec.lua
+++ automation/commands/cexec.lua
@@ -1,14 +1,14 @@
 local Command = require("Command")
 local Player = require("Player")
-local PermissionImmunityGREATER = require("Permission").Immunity.GREATER
+local Permission = require("Permission")
 
 local table_concat = table.concat
 
 Command:register{
 	name = "cexec",
 	run = function(self, ply, args)
-		local targets = Player:findSingle(self.makePlayerFindConstraint({
-			immunityRequirement = PermissionImmunityGREATER
+		local targets = Player:find(self.makePlayerFindConstraint({
+			immunityRequirement = Permission.Immunity.GREATER
 		}, args[1], ply, self))
 		if #targets < 1 then
 			ply:sendError("No appropriate target found")
