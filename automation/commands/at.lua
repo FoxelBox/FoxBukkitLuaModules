c363e1e8796fc773a4fabb36d0c248b4e4d583a9
Mark Dietzer <mriq91@gmail.com>
2015-07-10 01:36:24 +0200

    Make add support multicommand

--- automation/commands/at.lua
+++ automation/commands/at.lua
@@ -3,13 +3,19 @@ local Server = require("Server")
 
 local table_concat = table.concat
 
+local function sendMultiCommand(ply, reply)
+	for cmd in reply:gmatch('[^;]+') do
+		ply:chat(cmd)
+	end
+end
+
 Command:register{
 	name = "at",
 	run = function(self, ply, args)
 		local time = args[1]
 		local args = table_concat(args, " ", 2)
 		Server:runOnMainThread(function()
-			ply:chat(args)
+			sendMultiCommand(ply, args)
 		end, time * 20)
 		ply:sendReply("Command scheduled")
 	end
