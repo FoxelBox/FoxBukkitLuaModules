e9a1102696a2197b6b9ead54cb300be37ea62f08
Mark Dietzer <mriq91@gmail.com>
2016-01-01 12:00:41 +0100

    Add cap. Fix formatting

--- cheats/commands/potionbased.lua
+++ cheats/commands/potionbased.lua
@@ -4,21 +4,11 @@ local Event = require("Event")
 local PotionEffectType = bindClass("org.bukkit.potion.PotionEffectType")
 local PotionEffect = bindClass("org.bukkit.potion.PotionEffect")
 
-local function makePermanentPotionEffect(type, amp)	
+local POTION_EFFECT_CAP = 10
+
+local function makePermanentPotionEffect(type, amp)
 	return luajava.new(PotionEffect, type, 2147483647, amp or 0)
 end
-
-Event:registerReadOnlyPlayerJoin(function(ply)
-	if ply._effects then
-		for type, factor in next, ply._effects do
-			if factor and factor > 0 then
-				ply:addPotionEffect(makePermanentPotionEffect(type, factor), true)
-			else
-				ply:removePotionEffect(type)
-			end
-		end
-	end
-end)
 
 local function makePotionEffectCommand(name, type)
 	return Command:register{
@@ -52,7 +42,14 @@ local function makePotionEffectCommand(name, type)
 					factor = -factor
 				end
 			end
+
+			if factor > POTION_EFFECT_CAP or factor < -POTION_EFFECT_CAP then
+				ply:sendError("Sorry, potion effect cap is " .. POTION_EFFECT_CAP)
+				return
+			end
+
 			ply._effects[type] = factor
+
 			if factor > 0 then
 				ply:addPotionEffect(makePermanentPotionEffect(type, factor), true)
 				self:sendActionReply(ply, nil, {}, type:getName(), factor)
@@ -68,54 +65,62 @@ end
 
 makePotionEffectCommand("fullbright", PotionEffectType.NIGHT_VISION)
 makePotionEffectCommand("speed", PotionEffectType.SPEED)
+makePotionEffectCommand("jump", PotionEffectType.JUMP)
 
 Command:register{
-		name = "potion",
-		action = {
-			format = "%s set potion effect %s to %d",
-			isProperty = true
+	name = "potion",
+	action = {
+		format = "%s set potion effect %s to %d",
+		isProperty = true
+	},
+	arguments = {
+		{
+			name = "type",
+			type = "enum",
+			enum = PotionEffectType
 		},
-		arguments = {
-			{
-				name = "type",
-				type = "enum",
-				enum = PotionEffectType
-			},
-			{
-				name = "factor",
-				type = "number",
-				required = false,
-				default = false,
-				aliases = {
-					off = 0,
-					on = 1
-				}
+		{
+			name = "factor",
+			type = "number",
+			required = false,
+			default = false,
+			aliases = {
+				off = 0,
+				on = 1
 			}
-		},
-		run = function(self, ply, args)
-			local factor = args.factor
-			local type = args.type
+		}
+	},
+	run = function(self, ply, args)
+		local factor = args.factor
+		local type = args.type
 
-			if not ply._effects then
-				ply._effects = {}
-			end
-			if factor == false then
-				factor = ply._effects[type]
-				if not factor then
-					factor = 1
-				else
-					factor = -factor
-				end
-			end
-			ply._effects[type] = factor
-			if factor > 0 then
-				ply:addPotionEffect(makePermanentPotionEffect(type, factor), true)
-				self:sendActionReply(ply, nil, {}, type:getName(), factor)
+		if not ply._effects then
+			ply._effects = {}
+		end
+		if factor == false then
+			factor = ply._effects[type]
+			if not factor then
+				factor = 1
 			else
-				ply:removePotionEffect(type)
-				self:sendActionReply(ply, nil, {
-					format = "%s disabled potion effect %s"
-				}, type:getName())
+				factor = -factor
 			end
 		end
-	}
+
+		if factor > POTION_EFFECT_CAP or factor < -POTION_EFFECT_CAP then
+			ply:sendError("Sorry, potion effect cap is " .. POTION_EFFECT_CAP)
+			return
+		end
+
+		ply._effects[type] = factor
+
+		if factor > 0 then
+			ply:addPotionEffect(makePermanentPotionEffect(type, factor), true)
+			self:sendActionReply(ply, nil, {}, type:getName(), factor)
+		else
+			ply:removePotionEffect(type)
+			self:sendActionReply(ply, nil, {
+				format = "%s disabled potion effect %s"
+			}, type:getName())
+		end
+	end
+}
