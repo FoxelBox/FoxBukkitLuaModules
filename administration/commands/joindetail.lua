707f183943172e22f17756afcae406a83d02e571
Zidonuke <zidonuke@gmail.com>
2016-01-20 00:38:34 -0500

    It works! Already on server too.

--- administration/commands/joindetail.lua
+++ administration/commands/joindetail.lua
@@ -7,9 +7,9 @@ local table_concat = table.concat
 
 Event:registerReadOnlyPlayerJoin(function(ply)
 	local admintools = {}
-	table_insert(admintools, "Admin Tools: " .. ply:getDisplayName())
-	table_insert(admintools, Chat:makeButton("/ipinfo " .. ply:getName(), "[IP]", "cyan", true, false))
-	table_insert(admintools, Chat:makeButton("/mute " .. ply:getName(), "[Mute]", "gold", true, false))
-	table_insert(admintools, Chat:makeButton("/ban " .. ply:getName() .. " ", "[Ban]", "red", false, false))
+	table_insert(admintools, "<color name=\"dark_purple\">(FBAT)</color> " .. ply:getDisplayName())
+	table_insert(admintools, Chat:makeButton("/ipinfo " .. ply:getName(), "IP", "aqua", true, false))
+	table_insert(admintools, Chat:makeButton("/mute " .. ply:getName(), "Mute", "gold", true, false))
+	table_insert(admintools, Chat:makeButton("/ban " .. ply:getName() .. " ", "Ban", "red", false, false))
 	Chat:sendLocalToPermission(table_concat(admintools, " "), "foxbukkit.opchat", nil)
 end)
