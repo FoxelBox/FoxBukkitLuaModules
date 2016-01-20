8b4b6f9a935782a66f4906645a86a60a937c245d
Zidonuke <zidonuke@gmail.com>
2016-01-20 01:05:15 -0500

    Untested has never joined before detail.

--- administration/commands/joindetail.lua
+++ administration/commands/joindetail.lua
@@ -7,7 +7,11 @@ local table_concat = table.concat
 
 Event:registerReadOnlyPlayerJoin(function(ply)
 	local admintools = {}
-	table_insert(admintools, "<color name=\"dark_purple\">(FBAT)</color> " .. ply:getDisplayName())
+	local played = ""
+	if not ply.hasPlayedBefore then
+		player = " has not played before."
+	end
+	table_insert(admintools, "<color name=\"dark_purple\">(FBAT)</color> " .. ply:getDisplayName() .. played)
 	table_insert(admintools, Chat:makeButton("/ipinfo " .. ply:getName(), "IP", "aqua", true, false))
 	table_insert(admintools, Chat:makeButton("/mute " .. ply:getName(), "Mute", "gold", true, false))
 	table_insert(admintools, Chat:makeButton("/ban " .. ply:getName() .. " ", "Ban", "red", false, false))
