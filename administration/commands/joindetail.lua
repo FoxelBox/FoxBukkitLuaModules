2f912a91ce9127826b0d07b6d43b8eb7885b6846
Zidonuke <zidonuke@gmail.com>
2016-01-20 00:24:11 -0500

    Lol don't kill me please, this is horrible and non functional most likely.

---
+++ administration/commands/joindetail.lua
@@ -0,0 +1,15 @@
+local Player = require("Player")
+local Event = require("Event")
+local Chat = require("Chat")
+
+local table_insert = table.insert
+local table_concat = table.concat
+
+Event:registerReadOnlyPlayerJoin(function(ply)
+	local admintools = {}
+	table_insert(admintools, "Admin Tools: " .. ply:getDisplayName())
+	table_insert(admintools, Chat:makeButton("/ipinfo " .. ply:getName(), "[IP]", "cyan", true, false))
+	table_insert(admintools, Chat:makeButton("/mute " .. ply:getName(), "[Mute]", "gold", true, false))
+	table_insert(admintools, Chat:makeButton("/ban " .. ply:getName() .. " ", "[Ban]", "red", false, false))
+	Chat:sendLocalToPermission(table_concat(admintools, " "), "foxbukkit.opchat", nil)
+end)
